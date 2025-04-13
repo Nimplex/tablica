627e8895a9ca7bfbe944da8b01b12e68df27d7a0
Nimplex <39964594+Nimplex@users.noreply.github.com>
2025-04-14 00:26:08 +0200

    timetable display

--- src/app/_components/Timetable.tsx
+++ src/app/_components/Timetable.tsx
@@ -4,8 +4,15 @@ import { PulseLoader } from 'react-spinners';
 
 import './Timetable.css';
 
+export interface TimetableEntry {
+  date: Date;
+  absentTeacher: string;
+  changes: string[];
+}
+
 export interface TimetableData {
   title: string;
+  entries: TimetableEntry[];
 }
 
 export default function Timetable({ data }: { data?: TimetableData }) {
@@ -13,6 +20,30 @@ export default function Timetable({ data }: { data?: TimetableData }) {
     <div className="timetable">
       <h1>{data.title}</h1>
       <span className="divider"></span>
+      <div className="list">
+        {data.entries.map((entry, i) => (
+          <div className="entry" key={i}>
+            <p>
+              <u>
+                {entry.date.toLocaleDateString('pl-PL', {
+                  day: 'numeric',
+                  month: 'numeric',
+                  year: 'numeric',
+                })}{' '}
+                ({entry.date.toLocaleDateString('pl-PL', { weekday: 'short' })})
+              </u>{' '}
+              - nb: <span style={{ color: 'red' }}>{entry.absentTeacher}</span>:
+            </p>
+            <ol>
+              {entry.changes.map((change, i) => (
+                <li key={i}>
+                  <p>{change}</p>
+                </li>
+              ))}
+            </ol>
+          </div>
+        ))}
+      </div>
     </div>
   ) : (
     <div className="loading">
