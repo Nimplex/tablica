ecb96587d2877974679f6e5d77bed77103a55ac1
Nimplex <39964594+Nimplex@users.noreply.github.com>
2025-04-16 21:11:48 +0200

    make website horizontaly scalable

--- src/app/_components/Announcements.tsx
+++ src/app/_components/Announcements.tsx
@@ -10,7 +10,7 @@ export default function Announcements() {
     <div className="announcements">
       <h1>📢 Głos dyrekcji</h1>
       <span className="divider"></span>
-      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      <div dangerouslySetInnerHTML={{ __html: content }} className="list"></div>
     </div>
   );
 }
